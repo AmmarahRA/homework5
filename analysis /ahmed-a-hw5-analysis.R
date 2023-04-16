afdc0ffdf574addcdec56b1cd069eaad1f39776a
Ammarah Ahmed <ammarah.rafia.ahmed@emory.edu>
2023-04-16 01:55:01 -0400

    attempt 1

--- analysis /ahmed-a-hw5-analysis.R
+++ analysis /ahmed-a-hw5-analysis.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata)
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, modelsummary)
 
 final.data <- read_tsv('data/output/acs_medicaid.txt')
 
@@ -34,35 +34,35 @@ mcaid_fig
 
 #4
 
-filt_data <- final.data %>% filter(expand_year == 2014) %>%
-  group_by(expand_ever, year) %>% summarise(mean=mean(perc_unins) 
+plot_data <- final.data %>% filter(expand_year == 2014 | is.na(expand_year), !is.na(expand_ever)) %>%
+  group_by(expand_ever, year) %>% summarise(mean=mean(perc_unins)) 
 
-
-
-final.data %>% filter(expand_year == 2014) %>% 
-  group_by(expand_ever, year) %>% 
-  summarise(avg_unins = mean(perc_unins)) %>%
-  ggplot(aes(x = year)) + 
-  geom_line(aes(x = year, y = avg_unins)) +
-  labs(title = "Share of Uninsured Population", x = "Year", y = "Fraction Uninsured") +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5)) 
-
-
-ins.plot.dat <- final.data %>% filter(expand_year == 2014) %>%
-  group_by(expand_ever, year) %>% summarise(mean=mean(perc_unins) 
- 
-unins.fig <-  ggplot(data=ins.plot.dat, aes(x=year,y=mean,
+unins.plot <- ggplot(data = plot_data, aes(x = year, y = mean, 
+                                           group = expand_ever,
+                                           linetype = expand_ever)) + 
   geom_line() + geom_point() + theme_bw() +
-geom_vline(xintercept=2013.5, color="red") +
-geom_text(data = ins.plot.dat %>% filter(year  2016)
- aes(label = c("Non-expansion","Expansion"),  x = year + 1, y = mean)) +
- guides(linetype="none") +
- labs( x="Year", y="Fraction Uninsured",title="Share of Uninsured over Time")                                                                                                  
-                                                                                                                                  
+  geom_vline(xintercept=2013.5, color="red") +
+  geom_text(data = plot_data %>% filter(year == 2016), 
+            aes(label = c("Non-expansion","Expansion"),
+                x = year + 1,
+                y = mean)) +
+  guides(linetype="none") +
+  labs(x = "Year", y = "Fraction Uninsured", title = "Share of Uninsured over Time") +
+  theme(plot.title = element_text(hjust = 0.5)) 
+unins.plot
 
+#5
 
+reg.data <- final.data %>% filter(expand_ever == 'TRUE' & (year == 2012 | year == 2015)) %>%
+  mutate(post = (year>=2014),
+         treat=post*expand_ever)
 
+m.dd.t <- lm(perc_unins ~ post + expand_ever + treat, data=reg.data)
+summary(m.dd.t)
 
+reg.data2 <- final.data %>% filter(expand_ever == 'FALSE' & (year == 2012 | year == 2015)) %>%
+  mutate(post = (year>=2014),
+         treat=post*expand_ever)
 
-
+m.dd.f <- lm(perc_unins ~ post + expand_ever + treat, data=reg.data2)
+summary(m.dd.f)
